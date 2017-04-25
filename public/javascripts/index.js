41ea72b2d5bd0df7cfd7b180c063a87a0e4128c1
TakanoriOnuma <s1000607@sendai-nct.jp>
2017-04-26 08:46:52 +0900

    仮送信フォームの作成

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,19 @@
 var socket = io.connect();
+socket.on('connect', function() {
+  console.log('connect:', socket.id);
+});
 socket.on('register', function(data) {
   $('#register').append(JSON.stringify(data) + '<br>');
+});
+
+$('form').submit(function(e) {
+  e.preventDefault();
+
+  var userId = $('#userId').val();
+  $.post({
+    url: '/v1/register',
+    data: {
+      userId: userId
+    }
+  });
 });
